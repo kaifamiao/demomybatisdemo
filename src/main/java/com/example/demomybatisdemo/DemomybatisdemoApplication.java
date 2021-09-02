5c41252e54aa27b4b705249727b829d07549facc
kaifamiaos <1002020905@qq.com>
2021-09-02 23:41:26 +0800

    Initial commit

---
+++ src/main/java/com/example/demomybatisdemo/DemomybatisdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomybatisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomybatisdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemomybatisdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demomybatisdemo/DemomybatisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomybatisdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
